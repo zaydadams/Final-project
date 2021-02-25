3b4445e095da47b12e749e92c5be62169b8cb4d2
zaydadams <zaydadams6@gmail.com>
2021-02-25 15:24:57 +0200

    updated

---
+++ static/js-folder/main.js
@@ -0,0 +1,29 @@
+//contact form script//
+
+const x =document.getElementById('login');
+const y =document.getElementById('register');
+const z =document.getElementById('btn');
+
+
+function register(){
+    x.style.display = "none";
+    y.style.display = "block"
+    x.style.left='400px';
+    y.style.left='50px'
+    z.style.left='110px'
+}
+function login(){
+    y.style.display = "none";
+    x.style.display = "block"
+    x.style.left='50px';
+    y.style.left='450px'
+    z.style.left='0px'
+}
+
+
+const modal = document.getElementById('login-form');
+window.onclick = function(event){
+    if (event.target == modal){
+        modal.style.display ="none";
+    }
+}
